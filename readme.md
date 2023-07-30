ff3901d43e421edfa068fd81c426323b98d490f5
HiroFr <d.anselmet@thomyris.com>
2023-07-30 22:46:35 +0200

    add: HiroToDo init

---
+++ readme.md
@@ -0,0 +1,23 @@
+# HiroToDo 🎉
+Une extension chrome pour une ToDo-List
+
+Design and Build by HiroFr with ❤️
+
+
+
+## Install via git
+```bash
+
+git clone https://github.com/HiroFr/HiroToDo.git
+
+```
+
+Open your browser then go to extension and click on **load unpacked** and **select HiroToDo folder**.
+
+## Install via zip
+
+- Download the zip file from the git repository "HiroToDo"
+
+- Unzip
+
+- Open your browser then go to extension and click on **load unpacked** and **select HiroToDo folder**.
---
+++ src/popup.js
@@ -0,0 +1,83 @@
+// Fonction pour récupérer les éléments depuis le localStorage
+function getItemsFromLocalStorage() {
+    const itemsJSON = localStorage.getItem("items");
+    return itemsJSON ? JSON.parse(itemsJSON) : [];
+}
+
+// Fonction pour sauvegarder les éléments dans le localStorage
+function saveItemsToLocalStorage(items) {
+    localStorage.setItem("items", JSON.stringify(items));
+}
+
+// Fonction pour ajouter un élément
+function addItem() {
+    const nameInput = document.getElementById("name");
+    const itemName = nameInput.value;
+
+    if (itemName.trim() !== "") {
+        const newItem = { name: itemName };
+        const items = getItemsFromLocalStorage();
+        items.push(newItem);
+        saveItemsToLocalStorage(items);
+        nameInput.value = "";
+        displayItems();
+    }
+}
+
+// Fonction pour afficher les éléments
+function displayItems() {
+    const itemList = document.getElementById("itemList");
+    itemList.innerHTML = "";
+
+    const items = getItemsFromLocalStorage();
+    items.forEach((item, index) => {
+        const addToDo = document.createElement("div");
+        addToDo.innerHTML = `
+        <div class="allElement"> <!-- note avec l'encoche, la zone de texte, le bouton supprimé et le bouton d'édition -->
+            <div class="zdtDiv"> <!-- Boite pour le texte -->
+                <span class="zdt">${item.name}</span> <!-- zone de texte -->
+            </div>
+            <div class="moreIcon">
+            <img class="iDelete" src="../assets/icon/delete.svg" alt="Supprimer"> <!-- icône Modifier -->
+            <img class="iEdit" src="../assets/icon/edit.svg" alt="Modifier"> <!-- icône Supprimer -->
+            </div>
+        </div>`;
+        itemList.appendChild(addToDo);
+    });
+
+    // Ajouter les gestionnaires d'événements pour les boutons Modifier et Supprimer
+    const editButtons = document.querySelectorAll(".iEdit");
+    editButtons.forEach((btn, index) => {
+        btn.addEventListener("click", () => editItem(index));
+    });
+
+    const deleteButtons = document.querySelectorAll(".iDelete");
+    deleteButtons.forEach((btn, index) => {
+        btn.addEventListener("click", () => deleteItem(index));
+    });
+}
+
+// Fonction pour éditer un élément
+function editItem(index) {
+    const newName = prompt("Modifier la note :");
+    if (newName !== null && newName.trim() !== "") {
+        const items = getItemsFromLocalStorage();
+        items[index].name = newName;
+        saveItemsToLocalStorage(items);
+        displayItems();
+    }
+}
+
+// Fonction pour supprimer un élément
+function deleteItem(index) {
+    const items = getItemsFromLocalStorage();
+    items.splice(index, 1);
+    saveItemsToLocalStorage(items);
+    displayItems();
+}
+
+// Appel initial pour afficher les éléments existants (s'il y en a)
+displayItems();
+
+// Ajouter le gestionnaire d'événement pour le bouton Ajouter en dehors de la fonction displayItems()
+document.getElementById("addItemBtn").addEventListener("click", addItem);
